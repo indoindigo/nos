c4b8bdf57fc05fb32cf474bae59dffa5898465b8
indoindigo <39295990+indoindigo@users.noreply.github.com>
2018-05-15 17:09:36 +0400

    Add files via upload

---
+++ s1.cs
@@ -0,0 +1,19 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System.Numerics;
+
+namespace RandomNumbers
+{
+    public class Contract : SmartContract
+    {
+        public static void Main()
+        {
+            long numberOfTickets = 666;
+            Header header = Blockchain.GetHeader(Blockchain.GetHeight());
+            long randomNumber = (long)(header.ConsensusData >> 32);
+            long winningTicket = (randomNumber * numberOfTickets) >> 32;
+            Runtime.Notify("The winning ticket is:", winningTicket);
+        }
+    }
+}
